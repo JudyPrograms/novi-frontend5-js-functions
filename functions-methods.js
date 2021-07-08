a0500a8393f65932efbdae02432320fd85f2683b
JudyPrograms <judithkats@gmail.com>
2021-07-08 12:15:24 +0200

    homework final

--- functions-methods.js
+++ functions-methods.js
@@ -1,36 +1,142 @@
-// Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
-// Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
+// Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten te checken. Je zult
+// over de cijfers heen moeten itereren (hoe pak je dat aan?),
+// maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe
+// je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
+// Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
+const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
-/* Opdracht  1 */
-// Schrijf een functie genaamd getEmailDomain, die een emailadres verwacht en de domeinnaam teruggeeft. Een domeinnaam is hetgeen dat na het @ in het adres staat
+/* Opdracht  1: Cum Laude */
+
+/* 1a: Script schrijven  */
+// De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten
+// namelijk speciale diploma's voor besteld worden.
+// Schrijf de stapjes om dit te kunnen checken eerst uit en vraag jezelf de volgende dingen af:
+// * Hoe kan ik iedere waarde van de array checken op deze conditie?
+// * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat?
+// * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
+// Log het antwoord in de terminal.
+
+function checkCumLaude(gradesList) {
+    let amountCumLaude = 0
+    for (let i = 0; i < gradesList.length; i++) {
+        if (gradesList[i] >= 8 ){
+            amountCumLaude += 1
+        }
+    }
+    return amountCumLaude
+}
+
+// ---- Verwachte uitkomst: 6
+
+console.log(checkCumLaude(grades))
+
+/*  1b: Omschrijven tot een herbruikbare functie   */
+// Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude
+// studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
+// Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld:
+// [6, 4, 5] of [8, 9, 4, 6, 10].
+// Log het antwoord in de terminal.
+
 // ---- Verwachte uitkomsten:
-// getEmailDomain("n.eeken@novi-education.nl") geeft novi-education.nl
-// getEmailDomain("t.mellink@novi.nl") geeft novi.nl
-// getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
+// cumLaude(grades) geeft 6
+// cumLaude([6, 4, 5]) geeft 0
+// cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+// Dit had ik hierboven al gedaan. Ik had dan nog 'gradesList' kunnen vervangen voor 'numbersArray' oid. Bij dezen
+// nog de twee overige verwachte uitkomsten:
+console.log(checkCumLaude([6, 4, 5]))
+console.log(checkCumLaude([8, 9, 4, 5, 10]))
 
+/* Opdracht  2: Gemiddeld cijfer */
 
+/* 2a: Script schrijven  */
+// De studenten-administratie moet ieder blok opnieuw berekenen wat het gemiddelde eindcijfer is, maar we beginnen met
+// de grades array van hierboven.
+// Schrijf de stapjes om dit te kunnen berekenen eerst uit en vraag jezelf de volgende dingen af:
+// * Hoe wordt een gemiddelde berekend?
+// * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
+// * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
+// Log het antwoord in de terminal.
 
-/* Opdracht  2 */
-// Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
+function calculateMean(gradesList) {
+    let sumGrades = 0
+    for (let i = 0; i < gradesList.length; i++) {
+        sumGrades += gradesList[i]
+    }
+    return sumGrades / gradesList.length
+}
+
+// ---- Verwachte uitkomst: 6.642857142857143
+
+console.log(calculateMean(grades))
+
+/* 2b: Omschrijven tot een herbruikbare functie */
+// Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer
+// teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
+// Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of
+// [8, 9, 4, 6, 10].
+// Log het antwoord in de terminal.
+
 // ---- Verwachte uitkomsten:
-// typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
-// typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
-// typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
-// typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
+// averageGrade(grades) geeft 6.642857142857143
+// averageGrade([6, 4, 5]) geeft xxxx, 5
+// averageGrade([8, 9, 4, 6, 10]) geeft xxxx, 7.4
+
+// Dit had ik hierboven al gedaan. Ik had dan nog 'gradesList' kunnen vervangen voor 'numbersArray' oid. Bij dezen
+// nog de twee overige verwachte uitkomsten:
+
+console.log(calculateMean([4, 5, 6]))
+console.log(calculateMean([8, 9, 4, 6, 10]))
+
+/* 2c: Afronden op twee decimalen */
+// Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
+// Tip: Google is your best friend!
+
+function calculateMeanRound(gradesList) {
+    let sumGrades = 0
+    for (let i = 0; i < gradesList.length; i++) {
+        sumGrades += gradesList[i]
+    }
+    const mean = sumGrades / gradesList.length
+    return mean.toFixed(2)
+}
+
+console.log(calculateMeanRound(grades))
 
 
+/* Bonusopdracht: hoogste cijfer */
 
-/* Opdracht  3 */
-// Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
-// Een emailadres is valide wanneer:
-// * Er een @ in voorkomt
-// * Er géén , in voorkomt
-// * Er géén . in voorkomt als allerlaatste karakter (dus hotmail.com is valide, net als outlook.nl, maar outlooknl. niet)
+/* 3a: Script schrijven  */
+// Schrijf een script die op basis van de grades array (hierboven) checkt wat het hoogst behaalde cijfer is. Je mag
+// hier geen bestaande methoden voor gebruiken. Schrijf de stapjes eerst uit en vraag jezelf de volgende dingen af:
+// * Hoe kan ik iedere waarde van de array langsgaan?
+// * Op welke conditie moet ik checken?
+// * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan opslaan?
+// Log het antwoord in de terminal.
+
+function getHighestNumber(list) {
+    let highestNumber = 0
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] > highestNumber) {
+            highestNumber = list[i]
+        }
+    }
+    return highestNumber
+}
+
+// ---- Verwachte uitkomst: 9
+
+console.log(getHighestNumber(grades))
+console.log(getHighestNumber([6, 4, 5]))
+console.log(getHighestNumber([8, 9, 4, 6, 10]))
+
+/* 3b: Omschrijven tot een herbruikbare functie */
+// Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
+// Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
+// Log het antwoord in de terminal.
+
 // ---- Verwachte uitkomsten:
-// checkEmailValidity("n.eeken@novi.nl") geeft true - want @ en punt op de juiste plek
-// checkEmailValidity("tessmellink@novi.nl") geeft true - want @ en punt op de juiste plek
-// checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
-// checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
-// checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+// highestGrade(grades) geeft 9
+// highestGrade([6, 4, 5]) geeft 6
+// highestGrade([8, 9, 4, 6, 10]) geeft 10
